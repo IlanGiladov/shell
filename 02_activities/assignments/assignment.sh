d0c65bcdbada30c2431ca135d985cb2c7ea38a83
IlanGiladov <ilanit2001@gmail.com>
2026-04-12 23:15:30 -0400

    Commiting stages 1 to 3

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,11 +28,14 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
-
+mv rawdata data
+cd data
+mv rawdata raw
 # 3. List the contents of the ./data/raw directory
-
+cd raw
+ls
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
 
