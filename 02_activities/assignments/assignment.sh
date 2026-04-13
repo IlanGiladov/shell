4de9b47123d130ac9fb2821f778b3db3e5554a52
IlanGiladov <ilanit2001@gmail.com>
2026-04-13 00:05:51 -0400

    chaning the spacing

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -26,7 +26,6 @@ unzip -q rawdata.zip
 
 ###########################################
 # Complete assignment here
-
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
